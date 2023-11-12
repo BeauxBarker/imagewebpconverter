b3c3a449234c06fa3c7d9c7a73ef3a29f11b113e
B Barker <33534620+BeauxBarker@users.noreply.github.com>
2023-11-12 08:59:09 -0800

    webp image converter

--- convert.py
+++ convert.py
@@ -1,16 +1,12 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_file
 import pandas as pd
 from PIL import Image
-import os
 import zipfile
 import io
 import uuid
 import requests
 
 app = Flask(__name__)
-
-# Create a directory to store the converted images
-os.makedirs("converted_images", exist_ok=True)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -21,31 +17,38 @@ def index():
         if uploaded_file.filename != '':
             csv_data = pd.read_csv(uploaded_file)
 
-            # Create a list to store the file names of the converted images
-            converted_filenames = []
+            # Create a list to store the converted images
+            converted_images = []
 
             # Add your conversion code here
             for index, row in csv_data.iterrows():
-                image_url = row['ImageURL']
+                image_url = row['Images']
 
                 # Fetch the image data from the URL
                 response = requests.get(image_url)
                 if response.status_code == 200:
                     image_data = response.content
 
-                    # Perform the image conversion to WebP format and save it
-                    converted_filename = f"converted_images/image_{str(uuid.uuid4())}.webp"
-                    with open(converted_filename, "wb") as img_file:
-                        img_file.write(image_data)
-                    converted_filenames.append(converted_filename)
+                    # Perform the image conversion to WebP format
+                    with Image.open(io.BytesIO(image_data)) as img:
+                        img_byte_io = io.BytesIO()
+                        img.save(img_byte_io, format='WebP')
 
-            # Create a zip file containing the converted images
-            with zipfile.ZipFile('converted_images.zip', 'w') as zipf:
-                for filename in converted_filenames:
-                    zipf.write(filename)
+                    # Add the converted image to the list
+                    converted_images.append(
+                        (img_byte_io.getvalue(), f"image_{index}.webp"))
 
-            # Send the zip file for download
-            return send_from_directory('', 'converted_images.zip', as_attachment=True)
+            # Create a ZIP archive in memory
+            zip_buffer = io.BytesIO()
+            with zipfile.ZipFile(zip_buffer, 'w', zipfile.ZIP_DEFLATED, False) as zipf:
+                for img_data, img_name in converted_images:
+                    zipf.writestr(img_name, img_data)
+
+            # Move the cursor to the beginning of the ZIP buffer
+            zip_buffer.seek(0)
+
+            # Serve the ZIP archive as a downloadable file
+            return send_file(zip_buffer, as_attachment=True, download_name='converted_images.zip', mimetype='application/zip')
 
     return render_template('index.html')
 
